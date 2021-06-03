ccfdfdb77dc06bcd5c98cb7bd2b46eca325183bc
JaiMadaan <77677308+JaiMadaan@users.noreply.github.com>
2021-06-03 11:34:24 +0530

    Add files via upload

---
+++ js/Question.js
@@ -0,0 +1,78 @@
+class Question {
+
+  constructor() {
+    this.title = createElement('h1')
+    this.input1 = createInput("Enter Your Name Here....");
+    this.input2 = createInput("Enter Correct Option No..");
+    this.button = createButton('Submit');
+    this.question = createElement('h3');
+    this.option1 = createElement('h4');
+    this.option2 = createElement('h4');
+    this.option3 = createElement('h4');
+    this.option4 = createElement('h4');
+  }
+
+  hide(){
+    this.title.hide();
+    this.input1.hide();
+    this.button.hide();
+    this.input2.hide();
+  }
+
+  display(){
+    this.title.html("MyQuiz Game");
+    this.title.position(350, 0);
+
+    this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+    this.question.position(150, 80);
+    this.option1.html("1: Everyone " );
+    this.option1.position(150, 100);
+    this.option2.html("2: Envelope" );
+    this.option2.position(150, 120);
+    this.option3.html("3: Estimate " );
+    this.option3.position(150, 140);
+    this.option4.html("4: Example" );
+    this.option4.position(150, 160);
+
+    this.input1.position(150, 230);
+    this.input2.position(350, 230);
+    this.button.position(290, 300);
+
+    this.button.mousePressed(()=>{
+      this.title.hide();
+      this.input1.hide();
+      this.input2.hide();
+      this.button.hide();
+      contestant.name = this.input1.value();
+      contestant.answer = this.input2.value();
+      contestantCount+=1;
+      gameState+=1;
+      contestant.index = contestantCount;
+      contestant.update();
+      gameState.update();
+      text("Result of the Quiz",340, 50);
+      text("----------------------------",320, 65);
+      if(allContestants !== undefined){
+        debugger;
+        var display_Answers = 230;
+        fill("Blue");
+        textSize(20);
+        text("*NOTE: Contestant who answered correct are highlighted in green color!",130,230);
+  
+        for(var plr in allContestants){
+          debugger;
+          var correctAns = "3";
+          if (correctAns === allContestants[plr].answer)
+            fill("Green")
+          else
+            fill("red");
+  
+          display_Answers+=30;
+          textSize(20);
+          text(allContestants[plr].name + ": " + allContestants[plr].answer, 250,display_Answers)
+        }
+      }
+      contestant.updateCount(contestantCount);
+    });
+  }
+}
